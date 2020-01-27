8b4ee09149449831e6604696f90b835496feb57e
Colin Wang <chenyangw233@gmail.com>
2020-01-27 21:34:23 +1100

    Finish course 168:

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './screens/category_meals_screen.dart';
 
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -30,7 +31,15 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
+      // },
+      // onUnknownRoute: (settings) {
+
+      // },
     );
   }
 }
